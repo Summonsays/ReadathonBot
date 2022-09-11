ec56befa43c2efdfe12e7a51e7443e5d44c1af06
Fente\Ethan <minimagikman@gmail.com>
2022-09-11 11:34:12 -0400

    updated DB code and added some fun help commands

--- tutorialBot/modules/Commands.cs
+++ tutorialBot/modules/Commands.cs
@@ -12,11 +12,73 @@ namespace tutorialBot.modules
     {
 
         private static Stopwatch stopWatch = new Stopwatch();
+        private static Stopwatch sprintTime = new Stopwatch();
         private tutorialBot.modules.DataBase DB = new DataBase();
+        private static commandRegistry allCommands = new commandRegistry();
+        private static int howMad = 0;
+        //in minutes
+        private static int sprintDuration = 0;
+
+        //private string[] commands = new string[] { "hi", "help", "bookCount", "testy", "whatstheanswertolifetheuniverseandeverything", "haveAdminPerm", "stats", "addPages", "addBook", "restUser" };
 
         public static void Start()
         {
             stopWatch.Start();
+            CustomCommand temp = new CustomCommand();
+            temp = new CustomCommand("addBook", "Enter a book you've read");
+            allCommands.register(temp);
+            temp = new CustomCommand("addPages", "Enter a number to add pages to your total");
+            allCommands.register(temp);
+            temp = new CustomCommand("bookCount", "By defualt returns number of books you have read. Add a username to it to find out how many books someone else has read");
+            allCommands.register(temp);
+            temp = new CustomCommand("haveAdminPerm", "This returns if you have elivated permissions");
+            allCommands.register(temp);
+            temp = new CustomCommand("help", "This is the help command, supply with second command for details");
+            allCommands.register(temp);
+            temp = new CustomCommand("hi", "Bots love attention too!");
+            allCommands.register(temp);
+            temp = new CustomCommand("resetUser", "WARNING! This will erase your stats!");
+            allCommands.register(temp);
+            temp = new CustomCommand("stats", "This returns your personal stats. Add a username to see other people's stats");
+            allCommands.register(temp);
+            temp = new CustomCommand("testy", "You won't like me when I'm angry!!!!!");
+            allCommands.register(temp);
+            temp = new CustomCommand("uptime", "Returns the time since bot was started");
+            allCommands.register(temp);
+            temp = new CustomCommand("whatstheanswertolifetheuniverseandeverything", "Did you want to ask another question?");
+            allCommands.register(temp);
+
+        }
+
+        public async void startSprint(int time)
+        {
+            sprintTime.Start();
+            await Task.Delay(time);
+            stopSprint();
+        }
+
+        public async void stopSprint()
+        {
+            if(sprintTime.IsRunning)
+            {
+                await ReplyAsync("sprint ended");
+                sprintTime.Reset();
+            }
+        }
+
+        [Command("help")]
+        public async Task help()
+        {
+            await ReplyAsync(allCommands.help());
+        }
+
+        [Command("help")]
+        public async Task help(string command)
+        {
+            if (command == "Alwaysafk")
+                await ReplyAsync("I can't help you, no one can.");
+            else
+                await ReplyAsync(allCommands.help(command));
         }
 
         private bool hasPermission(Discord.WebSocket.SocketGuildUser user)
@@ -31,22 +93,53 @@ namespace tutorialBot.modules
         public async Task UpTime()
         {
             TimeSpan ts = stopWatch.Elapsed;
-            int hours = (int)(ts.TotalSeconds/3600);
-            int minutes = (int)((ts.TotalSeconds / 60)%60);
+            int hours = (int)(ts.TotalSeconds / 3600);
+            int minutes = (int)((ts.TotalSeconds / 60) % 60);
             int seconds = (int)(ts.TotalSeconds % 60);
-            await ReplyAsync("Time since start is: "+ hours+ "Hr " + minutes+"min "+ seconds+ "secs");
+            await ReplyAsync("Time since start is: " + hours + "Hr " + minutes + "min " + seconds + "secs");
+        }
+        [Command("resetUser")]
+        public async Task resetUserData([Remainder] string text)
+        {
+            if (!hasPermission((Discord.WebSocket.SocketGuildUser)this.Context.User))
+                return;
+            if (text == "")
+            {
+                DB.resetUser(this.Context);
+                await ReplyAsync("User " + this.Context.User.Username + " was reset");
+            }
+            else
+            {
+                DB.resetUser(this.Context, text);
+                await ReplyAsync("User " + text + " was reset");
+            }
+
+        }
+        [Command("resetUser")]
+        public async Task resetUserData()
+        {
+            if (!hasPermission((Discord.WebSocket.SocketGuildUser)this.Context.User))
+                return;
+            DB.resetUser(this.Context);
+            await ReplyAsync("User " + this.Context.User.Username + " was reset");
+
         }
 
         [Command("addPages")]
-        public async Task addPages([Remainder]string text)
+        public async Task addPages([Remainder] string text)
         {
             int pages = 0;
-            if(text.IndexOf(' ')>-1)
-                text=text.Split(' ')[0];
+            if (text.IndexOf(' ') > -1)
+                text = text.Split(' ')[0];
             if (Int32.TryParse(text, out pages))
             {
-                DB.addPages(this.Context, pages);
-                await ReplyAsync("Added " + text+ " pages " + this.Context.User.Username);
+                if (pages < 0)
+                    await ReplyAsync("You can't unread pages... " + this.Context.User.Username);
+                else
+                {
+                    DB.addPages(this.Context, pages);
+                    await ReplyAsync("Added " + text + " pages " + this.Context.User.Username);
+                }
             }
             else
             {
@@ -54,37 +147,206 @@ namespace tutorialBot.modules
             }
         }
 
-        [Command("addBook")]
-        public async Task addPages(string title, string author, [Remainder] string text)
+        [Command("Finished")]
+        public async Task addBook(string input, [Remainder] string text)
         {
-            await ReplyAsync("Title: " + title + " By " + author +" whatever: " + text);
+            input = input + " " + text;
+            DB.addBook(this.Context, input);   
+            await ReplyAsync(input);
         }
+
         [Command("t")]
         public async Task t()
         {
-            await ReplyAsync(this.Context.User.Username + " has read " + DB.getPages(this.Context) + " pages");
+            await ReplyAsync(this.Context.User.Username + " has read " + DB.getPages(this.Context.User.Username) + " pages");
         }
 
         [Command("stats")]
         public async Task stats()
         {
-            await ReplyAsync(this.Context.User.Username + " has read " + DB.getPages(this.Context) +" pages");
+            int books = DB.getBooks(this.Context.User.Username);
+            int pages = DB.getPages(this.Context.User.Username);
+
+            string response = this.Context.User.Username + " has read " + books +" book";
+            if (books>1)
+            {
+                response += "s";
+            }
+            response += " and has read " + pages + " pages";
+            await ReplyAsync(response);
+        }
+        [Command("stats")]
+        public async Task stats(string userName)
+        {
+            await ReplyAsync(userName + " has read " + DB.getPages(userName) + " pages");
         }
 
-            [Command("test")]
+        [Command("haveAdminPerm")]
         public async Task test()
         {
-            if(hasPermission((Discord.WebSocket.SocketGuildUser)this.Context.User))
+            if (hasPermission((Discord.WebSocket.SocketGuildUser)this.Context.User))
                 await ReplyAsync("yes");
             else
                 await ReplyAsync("no");
         }
 
+        [Command("whatstheanswertolifetheuniverseandeverything")]
+        public async Task theQuestion()
+        {
+            await ReplyAsync("42");
+        }
+
+        [Command("testy")]
+        public async Task unhappy()
+        {
+            string angry = "O w O";
+            if (howMad == 1)
+                angry = ">.<!";
+            if (howMad == 2)
+                angry = "(┛ಠ_ಠ)┛彡┻━┻";
+            if (howMad == 3)
+                angry = "(ノಠ益ಠ)ノ彡┻━┻";
+            if (howMad == 4)
+                angry = "┬─┬ノ(º _ ºノ)";
+            howMad++;
+            if (howMad > 4)
+                howMad = 0;
+            await ReplyAsync(angry);
+        }
+
+        [Command("bookCount")]
+        public async Task bookCount()
+        {
+            await ReplyAsync(this.Context.User.Username + " has read " + DB.getBooks(this.Context.User.Username) + " books");
+        }
+
+        [Command("startSprint")]
+        public async Task startSprint(string timeStr)
+        {
+            int time = 0;
+            if (timeStr.IndexOf(' ') > -1)
+                timeStr = timeStr.Split(' ')[0];
+            if (Int32.TryParse(timeStr, out time))
+            {
+                if (time < 0)
+                    await ReplyAsync("No negative sprint times" + this.Context.User.Username);
+                else
+                {
+                    if (!sprintTime.IsRunning)
+                    {
+                        if(time> 35000)
+                            await ReplyAsync("This Sprint is too long");
+                        else
+                        {
+                            time = time*60000 ;
+                            sprintDuration = time;
+                            startSprint(time);
+                            await ReplyAsync("Sprint started for  " + timeStr + " minutes");
+                        }
+                    }
+                    else
+                        await ReplyAsync("Sprint already started");
+                }
+            }
+            else
+            {
+                await ReplyAsync("Invalid time entered: " + timeStr);
+            }
+        }
+        [Command("stopSprint")]
+        public async Task stopSprintCommand()
+        {
+            if (!hasPermission((Discord.WebSocket.SocketGuildUser)this.Context.User))
+                return;
+            stopSprint();
+        }
+        [Command("time")]
+        public async Task timeSinceStart()
+        {
+            await ReplyAsync(sprintTime.Elapsed.ToString());
+        }
+        [Command("timeLeft")]
+        public async Task timeLeft()
+        {
+            if(!sprintTime.IsRunning)
+                await ReplyAsync("No sprint currently running");
+            else
+            {
+                int temp = sprintDuration;// * 6000;
+                //seconds left
+                temp = (int)((temp - (int)sprintTime.Elapsed.TotalMilliseconds)/1000);
+                int hours = (int)(temp / 3600);
+                int minutes = (int)((temp / 60) % 60);
+                int seconds = (int)(temp % 60);
+                await ReplyAsync("Time left: "+hours+":"+minutes+":"+seconds);
+            }
+        }
 
         [Command("hi")]
         public async Task Hi()
         {
             await ReplyAsync("hello!");
         }
+
+        [Command("Paige")]
+        public async Task paige()
+        {
+            if(this.Context.User.Id== 184478513951145985)
+                await ReplyAsync("Paige is best wife!");
+            else if (this.Context.User.Id == 449345265598464001)
+                await ReplyAsync("You are awesome!");
+            else
+                await ReplyAsync("Paige is in charge! Don't mess with her!");
+        }
+
+
+    }
+
+    internal class commandRegistry
+    {
+        static IList<CustomCommand> registeredCommands = new List<CustomCommand>();
+
+        public void register(CustomCommand temp)
+        {
+            registeredCommands.Add(temp);
+        }
+
+        public bool isRegistered(CustomCommand temp)
+        {
+            return registeredCommands.Contains(temp);
+        }
+
+        public string help()
+        {
+            string temp = "The list of commands are as follows: ";
+            foreach (CustomCommand item in registeredCommands)
+            {
+                temp += "\n" + item.getCommand();
+            }
+            return temp;
+        }
+
+        public string help(string commandName)
+        {
+            foreach(CustomCommand item in registeredCommands)
+            {
+                if(item.getCommand()== commandName)
+                    return item.getHelp();
+            }
+            return "command not found";
+        }
+    }
+    internal class CustomCommand
+    {
+        string commandWord = "";
+        string helpMessage = "";
+        public CustomCommand() { }
+        public CustomCommand(string s1, string s2)
+        {
+            commandWord = s1;
+            helpMessage = s2;
+        }
+        public string getCommand() { return commandWord; }
+        public string getHelp() { return helpMessage; }
     }
 }
--- tutorialBot/modules/DataBase.cs
+++ tutorialBot/modules/DataBase.cs
@@ -16,7 +16,6 @@ namespace tutorialBot.modules
     {
         //temporary will replace with realDB
         string[] names = new string[10];
-        static IList<pagesRead> sprintUsers = new List<pagesRead>();
         string connectionString = ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString;
         //static pagesRead[] sprintUsers = new pagesRead[10];
         public DataBase()
@@ -26,7 +25,7 @@ namespace tutorialBot.modules
 
 
 
-       /*testing DB*/
+       /*testing DB
         private void createConnection()
         {
             using var con = new NpgsqlConnection(connectionString);
@@ -39,80 +38,171 @@ namespace tutorialBot.modules
                 var version = cmd.ExecuteScalar().ToString();
                 Console.WriteLine($"PostgreSQL version: {version}");
 
-        }
+        }*/
 
         private void createUser(SocketUser userContext)
         {
-            using var con = new NpgsqlConnection(connectionString);
+            var con = new NpgsqlConnection(connectionString);
             con.Open();
-            var sql = "SELECT * from \"RATDB\".users where discordID=" + userContext.Id;
-            using var cmd = new NpgsqlCommand(sql, con);
+            var sql = "SELECT * from rat_db.users where discord_id=" + userContext.Id;
+            var cmd = new NpgsqlCommand(sql, con);
 
-            cmd.CommandText = "INSERT INTO \"RATDB\".users(discordID, discordName, creation, lastModified) VALUES(" + userContext.Id+",'"+userContext.Username+ "',:datetimeParamName,:datetimeParamName)";
+            cmd.CommandText = "INSERT INTO rat_db.users(discord_id, discord_name, creation, lastModified) VALUES(" + userContext.Id+",'"+userContext.Username+ "',:datetimeParamName,:datetimeParamName)";
 
             NpgsqlParameter param = new NpgsqlParameter(":datetimeParamName", NpgsqlTypes.NpgsqlDbType.Timestamp);
             param.Value = DateTime.Now;
             cmd.Parameters.Add(param); 
             Console.WriteLine(cmd.CommandText);
             cmd.ExecuteNonQuery();
+            con.Close();
         }
 
-
-        private pagesRead lookUpUser(SocketUser userContext)
+        private userInfo lookUpUser(SocketUser userContext)
         {
             using var con = new NpgsqlConnection(connectionString);
             con.Open();
-            var sql = "SELECT * from \"RATDB\".users where discordID="+userContext.Id;
+            var sql = "SELECT * from rat_db.users where discord_id="+userContext.Id;
             using var cmd = new NpgsqlCommand(sql, con);
             using NpgsqlDataReader rdr = cmd.ExecuteReader();
+            userInfo temp = new userInfo();
 
-            if (rdr.HasRows)
-            {
-                //read the user data
-            }
-            else
+
+
+            if (!rdr.HasRows)
             {
                 createUser(userContext);
+                return temp = new userInfo(userContext.Id, userContext.Username);
                 //create user in DB
             }
             while (rdr.Read())
             {
                 Console.WriteLine("{0} {1} {2} {3} {4}", rdr.GetInt32(0), rdr.GetInt64(1), rdr.GetString(2), rdr.GetDateTime(3), rdr.GetDateTime(4));
+                temp = new userInfo((ulong)rdr.GetInt64(1), rdr.GetString(2));
             }
+            con.Close();
 
-            foreach (pagesRead user in sprintUsers)
+            return temp;
+        }
+
+        internal void resetUser(SocketCommandContext context, string name="default")
+        {
+            if (name == "default")
+                name = context.User.Username;
+            userInfo tempUser = lookUpUserByName(name);
+            if (tempUser.getUserID() == 0)//name not found
+                return;
+
+            using var con = new NpgsqlConnection(connectionString);
+            con.Open();
+            var sql = "delete from rat_db.read_pages where user_id =" + tempUser.getUserID();
+            using var cmd = new NpgsqlCommand(sql, con);
+            using NpgsqlDataReader rdr = cmd.ExecuteReader();
+        }
+        private userInfo lookUpUserByName(string userName)
+        {
+            userInfo temp = new userInfo();
+            using var con = new NpgsqlConnection(connectionString);
+            con.Open();
+            var sql = "SELECT * from rat_db.users where discord_name='" + userName+"'";
+            using var cmd = new NpgsqlCommand(sql, con);
+            using NpgsqlDataReader rdr = cmd.ExecuteReader();
+            if (!rdr.HasRows)
             {
-                ulong id = userContext.Id;
-                if (user.getUserID() == userContext.Id)
-                    return user;
+                return temp = new userInfo(0, userName);
+                //create user in DB
             }
-            //could not find user
-            pagesRead temp = new pagesRead(userContext.Id, userContext.Username);
-            sprintUsers.Add(temp);
+            while (rdr.Read())
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4}", rdr.GetInt32(0), rdr.GetInt64(1), rdr.GetString(2), rdr.GetDateTime(3), rdr.GetDateTime(4));
+                temp = new userInfo((ulong)rdr.GetInt64(1), rdr.GetString(2));
+            }
+            con.Close();
             return temp;
         }
 
         internal void addPages(SocketCommandContext context, int pages)
         {
-            pagesRead userObj = lookUpUser(context.User);
-            userObj.addPages(pages);
+            userInfo userObj = lookUpUser(context.User);
+            var con = new NpgsqlConnection(connectionString);
+            con.Open();
+            var sql = "Insert into rat_db.read_pages (user_id, createts, modifiedts, pages_read) VALUES(" + userObj.getUserID()+",:datetimeParamName,:datetimeParamName,"+pages+")";
+            NpgsqlParameter param = new NpgsqlParameter(":datetimeParamName", NpgsqlTypes.NpgsqlDbType.Timestamp);
+            var cmd = new NpgsqlCommand(sql, con);
+            param.Value = DateTime.Now;
+            cmd.Parameters.Add(param);
+            Console.WriteLine(cmd.CommandText);
+            cmd.ExecuteNonQuery();
+            con.Close();
         }
 
-        public int getPages(SocketCommandContext context)
+        internal void addBook(SocketCommandContext context, string title)
         {
-            pagesRead userObj = lookUpUser(context.User);
-            return userObj.getPages();
+            userInfo userObj = lookUpUser(context.User);
+            var con = new NpgsqlConnection(connectionString);
+            con.Open();
+            title = "'" + title + "'";
+            var sql = "INSERT INTO rat_db.read_books(user_id, book_title, book_author, createts, modifiedts) VALUES(" + userObj.getUserID()+","+title+ ", null,:datetimeParamName,:datetimeParamName)"; 
+            NpgsqlParameter param = new NpgsqlParameter(":datetimeParamName", NpgsqlTypes.NpgsqlDbType.Timestamp);
+            var cmd = new NpgsqlCommand(sql, con);
+            param.Value = DateTime.Now;
+            cmd.Parameters.Add(param);
+            Console.WriteLine(cmd.CommandText);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public int getPages(string userName)
+        {
+            userInfo userObj = lookUpUserByName(userName);
+            using var con = new NpgsqlConnection(connectionString);
+            con.Open();
+            var sql = "SELECT * from  rat_db.read_pages where user_id=" + userObj.getUserID();
+            using var cmd = new NpgsqlCommand(sql, con);
+            using NpgsqlDataReader rdr = cmd.ExecuteReader();
+            int pages = 0;
+            if (!rdr.HasRows)
+            {
+                return 0;
+            }
+            while (rdr.Read())
+            {
+                pages += rdr.GetInt32(4);
+                //Console.WriteLine("{0} {1} {2} {3} {4}", rdr.GetInt32(0), rdr.GetInt64(1), rdr.GetString(2), rdr.GetDateTime(3), rdr.GetDateTime(4));
+            }
+            con.Close();
+
+            return pages;
+        }
+        public int getBooks(string userName)
+        {
+            userInfo userObj = lookUpUserByName(userName);
+            using var con = new NpgsqlConnection(connectionString);
+            con.Open();
+            var sql = "SELECT * from  rat_db.read_books where user_id=" + userObj.getUserID();
+            using var cmd = new NpgsqlCommand(sql, con);
+            using NpgsqlDataReader rdr = cmd.ExecuteReader();
+            int books = 0;
+            if (!rdr.HasRows)
+            {
+                return 0;
+            }
+            while (rdr.Read())
+            {
+                books++;
+            }
+            con.Close();
+
+            return books;
         }
     }
 
-    internal class pagesRead
+    internal class userInfo
     {
         int pages=0;
         ulong userID = 0;
         string username="";
 
-        public pagesRead() { }
-        public pagesRead(ulong userIDTemp, string user)
+        public userInfo() { }
+        public userInfo(ulong userIDTemp, string user)
         {
             userID = userIDTemp;
             username = user;   
